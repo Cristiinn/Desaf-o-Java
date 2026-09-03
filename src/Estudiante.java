e857f3259fa5d5a1ad11abcf48425d3266320227
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-09-02 21:35:18 -0400

    JAVA-006 - Implementar Main y crear Estudiante

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,15 +1,16 @@
-public class estudiante {
+public class Estudiante {
 
     private String nombre;
     private String carrera;
     private int edad;
 
 
-    public estudiante(String nombre, String carrera, int edad) {
+    public Estudiante(String nombre, String carrera, int edad) {
         this.nombre = nombre;
         this.carrera = carrera;
         this.edad = edad;
     }
+
 
     public String getNombre() {
         return nombre;
--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,11 @@
 public class Main {
     public static void main(String[] args) {
 
+    Estudiante estudianteUno = new Estudiante("juanito","Informatica", 21);
+
+    estudianteUno.mostrarInformacion();
+
+
+
     }
 }
